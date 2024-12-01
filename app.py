a77ea3ad1ab30f4dde3217b1adc1619967d6a997
AnFlGue <mokshaflores@icloud.com>
2024-12-01 16:02:27 +0100

    Get all characters and paginate them.  C_R_UD

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+from unittest.mock import patch
+
+from flask import Flask, request, jsonify
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Loading JSON data into a list.
+with open("characters.json", "r") as file:
+    characters = json.load(file)
 
-if __name__ == '__main__':
-    app.run()
+
+# Get all characters and paginate them.
+@app.route("/characters", methods=["GET"])
+def get_characters():
+    limit = int(request.args.get("limit", 2))
+    skip = int(request.args.get("skip", 0))
+    paginated = []
+    
+    end_index = min(skip + limit, len(characters))
+    
+    for i in range(skip, end_index):
+        paginated.append(characters[i])
+
+    return jsonify(paginated), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+from unittest.mock import patch
+
 from flask import Flask, request, jsonify
+import json
+
 app = Flask(__name__)
 
-# Loading JSON data into an in-memory Python list.
+# Loading JSON data into a list.
 with open("characters.json", "r") as file:
     characters = json.load(file)
 
-# Get all characters and paginate them.  C_R_UD
+# Get all characters and paginate them.
 @app.route("/characters", methods=["GET"])
 def get_characters():
     limit = int(request.args.get("limit", 2))
     skip = int(request.args.get("skip", 0))
     paginated = []
+    
     end_index = min(skip + limit, len(characters))
+    
     for i in range(skip, end_index):
         paginated.append(characters[i])
+    print(paginated)
     return jsonify(paginated), 200
 
-
 if __name__ == "__main__":
+    with patch('flask.request') as mock_request:
+        mock_request.args = {'limit': '2', 'skip': '0'}
+        get_characters()
     app.run(debug=True)
